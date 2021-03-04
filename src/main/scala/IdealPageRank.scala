effc877f0779069886d6c56807ca4d32b433eb80
Sushmitha Jayaramaiah <jayaramsushmitha1@gmail.com>
2021-03-04 10:02:20 -0700

    Update IdealPageRank.scala

--- src/main/scala/IdealPageRank.scala
+++ src/main/scala/IdealPageRank.scala
@@ -7,15 +7,24 @@ object IdealPageRank{
       System.err.println("Input not Provided")
       System.exit(1)
   }
-
+    // Creating a sprak session
     val spark = SparkSession.builder.master("yarn").getOrCreate()
     val sc = spark.sparkContext
+    
+    // Reading in the wikipedia links file which is of the format- "from1 : to3, to11, to100"
     val lines = spark.read.textFile(args(0)).rdd
+    
+    // Reading corresponding titles and adding index to them starting from 1
     val titles = spark.read.textFile(args(1)).rdd.zipWithIndex().mapValues(x=>x+1).map(_.swap).map{case(i,title) => (i.toString,title)}
+    
+    // Count the number of pages
     val totalPages = titles.count()
+    
+    //Split the from and to links and put into key and value form
     val links = lines.map(s => (s.split(":")(0), s.split(": ")(1).split(" ")))
+    // Initialize ranks to all the links
     var ranks = links.mapValues(v => 1.0 / totalPages)
-
+    //Iteratively calculate the pageRank
     for (i <- 1 to 25) {
       val tRank = links.join(ranks).values.flatMap {
         case (urls, rank) =>
@@ -25,8 +34,11 @@ object IdealPageRank{
 
       ranks = tRank.reduceByKey(_ + _)
     }
+    // Join the titles with the ranks of the links
     val titleRanks = titles.join(ranks).values
+    //Sort it in decreasing order and take the top 10
     val sortedOutput = titleRanks.sortBy(_._2,false).take(10)
+    //Save it as a text file
     sc.parallelize(sortedOutput.toSeq).coalesce(1,shuffle = true).sortBy(_._2,false).saveAsTextFile(args(2))
     spark.stop()
   }
